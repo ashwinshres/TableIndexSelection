43f0f37a980b8f20a1bb3fa59edcee4066ebcd07
Ashwin shrestha <shrashwiniw@gmail.com>
2019-07-15 17:23:39 +0545

    Initial Commit

---
+++ TableIndexSelection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableIndexSelection
+//
+//  Created by Insight Workshop on 7/15/19.
+//  Copyright © 2019 Skyway Innovation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
